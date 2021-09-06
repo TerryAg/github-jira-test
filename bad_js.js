dcafbdbf5d5ea9b9ec91b19ac2fd7e749dae27b3
Terry Agapitos <tagapitos@atlassian.com>
2021-09-06 16:17:49 +1000

    GH-8 - bad code gone

--- bad_js.js
+++ bad_js.js
@@ -1,10 +1,3 @@
 var app = require('express')();
+console.log("bad code gone");
 
-app.get('/user/:id', function(req, res) {
-  if (!isValidUserId(req.params.id))
-    // BAD: a request parameter is incorporated without validation into the response
-    res.send("Unknown user: " + req.params.id);
-  else
-    // TODO: do something exciting
-    ;
-});
