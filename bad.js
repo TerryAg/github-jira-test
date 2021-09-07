772ce65ddf7019ef50a319bafcac97e8e7b25579
Terry Agapitos <tagapitos@atlassian.com>
2021-09-07 12:31:22 +1000

    GH-9 - reintroduce

--- bad.js
+++ bad.js
@@ -1,5 +1,6 @@
 function setLanguageOptions() {
-    var href = document.location.href;
-   var   deflt = href.substring(href.indexOf("default=")+8);
-
+    var href = document.location.href,
+        deflt = href.substring(href.indexOf("default=")+8);
+    document.write("<OPTION value=1>"+deflt+"</OPTION>");
+    document.write("<OPTION value=2>English</OPTION>");
 }
