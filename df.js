26a75dc9021c843b4615f8365317eff362b7672c
Terry Agapitos <tagapitos@atlassian.com>
2022-02-03 14:19:50 +1100

    TP-45 - remove vuln

--- df.js
+++ df.js
@@ -1,10 +1,6 @@
 var app = require('express')();
 
 app.get('/ussssser/:id', function(req, res) {
-  if (!isValidUserId(req.params.id))
-    // BAD: a request parameter is incorporated without validation into the response
-    res.send("Unknown user: " + req.params.id);
-  else
     // TODO: do something exciting
-    ;
+    console.log('hi');
 });
